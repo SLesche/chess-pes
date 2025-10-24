e2cffbc9ccc8d78e73cb98f56fbd8e935fa4a305
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-10-24 16:38:21 +0200

    prep for large data

--- lichess_data_prep.R
+++ lichess_data_prep.R
@@ -8,7 +8,7 @@ pgn_file <- "eval_games_21_06_blitz.pgn"
 
 # test <- read_pgn_moves(pgn_file, max.lines = 100000
 con <- file(pgn_file, open = "r")
-games <- read_pgn_moves_stream(con, max.games = 100000)  # only first 1000 games
+games <- read_pgn_moves_stream(con, max.games = 350000)  # only first 1000 games
 close(con)
 
 data <- games %>% 
@@ -70,14 +70,22 @@ data <- data %>%
 
 data <- data %>% 
   mutate(
-    mistake = factor(ifelse(corrected_move_eval < -2 & ((color == "black" & eval > 0) | (color == "white" & eval < 0)), 1, 0))
+    mistake = factor(ifelse(corrected_move_eval < -2, 1, 0)),
+    sign_flipped = ifelse(prev_eval * eval < 0, 1, 0),
+    player_losing_before_move = ifelse(corrected_prev_eval < 0, 1, 0),
+    player_losing_after_move = ifelse(corrected_eval < 0, 1, 0)
   ) %>% 
   group_by(id) %>% 
   mutate(
     prev_opp_mistake = factor(lag(mistake))
   ) %>% 
   group_by(id, color) %>% 
-  mutate(prev_own_mistake = factor(lag(mistake))) %>% 
+  mutate(
+    prev_own_mistake = factor(lag(mistake)),
+    mistake_made_in_losing_position = factor(lag(player_losing_before_move)),
+    mistake_result_in_losing_position = factor(lag(player_losing_after_move)),
+    mistake_flipped_sign = factor(lag(sign_flipped))
+    ) %>% 
   ungroup() 
 
 data <- data %>% 
@@ -89,9 +97,19 @@ data <- data %>%
   filter(!clock > 18000) %>% 
   filter(move_time < 6000, move_time > 0) %>% 
   mutate(group = ifelse(prev_own_mistake == 1, "posterror", "control")) %>% 
-  mutate(clock_secs = clock / 100, opp_clock_secs = opp_clock / 100)
+  mutate(clock_secs = clock / 100, opp_clock_secs = opp_clock / 100) %>% 
+  mutate(
+    move_num_dec = round(move_num, -1),
+    clock_secs_dec = round(clock_secs, -1),
+    prev_eval_one = round(prev_eval),
+    prev_opp_move_eval_one = round(prev_opp_move_eval),
+    opp_clock_secs_dec = round(opp_clock_secs, -1),
+    opp_move_time_secs = opp_move_time / 100,
+    opp_move_time_secs_dec = round(opp_move_time / 100, -1),
+    move_time_secs = move_time / 100,
+  )
 
-saveRDS(data, file = "lichess_data/data_before_match.RDS")
+saveRDS(data, file = "lichess_data/data_before_match_350k_blitz.RDS")
 
 # 
 # posterror_move_data <- data %>% 
@@ -101,14 +119,68 @@ saveRDS(data, file = "lichess_data/data_before_match.RDS")
 
 # Example: match sampled_data to full_data by move_num and eval
 
-match_result <- matchit(
-  group ~ move_num + prev_eval + clock_secs + color + prev_opp_move_eval +  opp_move_time + opp_clock_secs, 
-  data = data,
-  method = "nearest",
-  distance = "mahalanobis"
-)
+# match_result <- matchit(
+#   group ~ move_num + prev_eval + clock_secs + color + prev_opp_move_eval +  opp_move_time + opp_clock_secs,
+#   data = data,
+#   method = "nearest",
+#   distance = "mahalanobis"
+# )
+# full_data <- match.data(match_result)
 
-full_data <- match.data(match_result)
+library(parallel)
+batch_ids <- split(data,
+                   interaction(
+                     data$color,
+                     data$move_num_dec,
+                     # data$prev_eval_one,
+                     # data$prev_opp_move_eval_one
+                     data$clock_secs_dec
+                     # data$opp_clock_secs_dec,
+                     # data$opp_move_time_secs_dec
+                     )
+                   )  # or any grouping
 
-saveRDS(match_result, file = "lichess_data/match_result.RDS")
-saveRDS(full_data, file = "lichess_data/full_data.RDS")
+batch_ids <- batch_ids[sapply(batch_ids, nrow) > 100]
+batch_ids <- batch_ids[sapply(batch_ids, function(df) any(df$group == "posterror"))]
+
+rm(data)
+gc()
+cores <- detectCores() - 1
+cl <- makeCluster(cores)
+
+# Export needed packages and objects to each worker
+clusterEvalQ(cl, library(MatchIt))
+clusterExport(cl, varlist = c("batch_ids"))
+
+# Run MatchIt in parallel safely on Windows
+results <- parLapply(cl, batch_ids, function(df) {
+  matchit(group ~ move_num + prev_eval + clock_secs +
+            prev_opp_move_eval + opp_move_time + opp_clock_secs,
+          data = df,
+          method = "nearest",
+          distance = "mahalanobis")
+})
+
+stopCluster(cl)
+
+# Extract matched datasets from each batch
+matched_dfs <- lapply(results, function(m) {
+  if (!is.null(m)) {
+    match.data(m)
+  } else {
+    NULL
+  }
+})
+
+rm(results)
+gc()
+# Remove any NULLs (safety)
+matched_dfs <- matched_dfs[!sapply(matched_dfs, is.null)]
+full_data <- bind_rows(matched_dfs, .id = "batch")
+rm(matched_dfs)
+gc()
+
+# saveRDS(match_result, file = "lichess_data/match_result.RDS")
+saveRDS(full_data, file = "lichess_data/full_data_350k_blitz.RDS")
+rm(full_data)
+gc()
