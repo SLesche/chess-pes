969bf1a3b79b15430d8f4894e78f9d596b3490c3
Sven Lesche <113923517+SLesche@users.noreply.github.com>
2025-10-29 11:35:05 +0100

    make moderator plots

--- helper_functions.R
+++ helper_functions.R
@@ -1,4 +1,4 @@
-plot_moderator_analysis <- function(data, moderator_var, include_error_types = FALSE){
+plot_moderator_analysis <- function(data, moderator_var, x_label = NULL, include_error_types = FALSE){
   # capture the unquoted variable
   mod_quo <- enquo(moderator_var)
   mod_name <- quo_name(mod_quo)
@@ -12,13 +12,17 @@ plot_moderator_analysis <- function(data, moderator_var, include_error_types = F
       group_by(moderator, mistake_made_in_losing_position_posterror, mistake_flipped_sign_posterror) %>% 
       summarize(
         mean_pes = mean(pes, na.rm = TRUE),
+        mean_se_pes = sd(pes, na.rm = TRUE) / sqrt(n()),
         sd_pes = sd(pes, na.rm = TRUE),
         mean_pea = mean(pea, na.rm = TRUE),
         effsize = effectsize::cohens_d(move_time_control, move_time_posterror)
       ) %>% 
+      mutate(
+        mean_upper = mean_pes + 1.96*mean_se_pes,
+        mean_lower = mean_pes - 1.96*mean_se_pes
+      ) %>% 
       janitor::clean_names()
     
-    colnames(eff_data) <- c("moderator", "mistake_in_losing_position_posterror", "sign_flipped_posterror", "mean_pes", "sd_pes", "mean_pea", "cohen_d")
   } else {
     eff_data <- data %>% 
       mutate(
@@ -27,46 +31,56 @@ plot_moderator_analysis <- function(data, moderator_var, include_error_types = F
       group_by(moderator) %>%
       summarize(
         mean_pes = mean(pes, na.rm = TRUE),
+        mean_se_pes = sd(pes, na.rm = TRUE) / sqrt(n()),
         sd_pes = sd(pes, na.rm = TRUE),
         mean_pea = mean(pea, na.rm = TRUE),
         effsize = effectsize::cohens_d(move_time_control, move_time_posterror)
       ) %>% 
+      mutate(
+        mean_upper = mean_pes + 1.96*mean_se_pes,
+        mean_lower = mean_pes - 1.96*mean_se_pes
+      ) %>% 
       janitor::clean_names()
     
-    colnames(eff_data) <- c("moderator", "mean_pes", "sd_pes", "mean_pea", "cohen_d")
   }
   
   
   # find actual moderator values at every 10th quantile
   quantile_labels <- data %>%
-    summarize(q = quantile(!!mod_quo, probs = seq(0.1, 1, 0.1), na.rm = TRUE)) %>%
+    summarize(q = quantile(!!mod_quo, probs = seq(0.05, 0.95, 0.1), na.rm = TRUE)) %>%
     pull(q)
   
   if (include_error_types){
     ggplot(eff_data, aes(x = moderator, y = mean_pes, group = sign_flipped_posterror, color = sign_flipped_posterror)) +
       geom_point(alpha = 0.7) +
+      geom_errorbar(
+        aes(ymin = mean_lower, ymax = mean_upper)
+      )+
       geom_smooth(se = FALSE) +
       scale_x_continuous(
-        breaks = seq(10, 100, by = 10),
+        breaks = seq(5, 95, by = 10),
         labels = round(quantile_labels, 2)
       ) +
       theme_classic() +
       labs(
         y = "PES",
-        x = glue::glue("{mod_name} (quantile bins → actual values on axis)")
+        x = ifelse(is.null(x_label), glue::glue("{mod_name} (quantile bins → actual values on axis)"), x_label)
       )
   } else {
     ggplot(eff_data, aes(x = moderator, y = mean_pes)) +
       geom_point() +
+      geom_errorbar(
+        aes(ymin = mean_lower, ymax = mean_upper)
+      )+
       geom_smooth(se = FALSE) +
       scale_x_continuous(
-        breaks = seq(10, 100, by = 10),
+        breaks = seq(5, 95, by = 10),
         labels = round(quantile_labels, 2)
       ) +
       theme_classic() +
       labs(
         y = "PES",
-        x = glue::glue("{mod_name} (quantile bins → actual values on axis)")
+        x = ifelse(is.null(x_label), glue::glue("{mod_name} (quantile bins → actual values on axis)"), x_label)
       )
   }
 }
--- pes_paper_results.R
+++ pes_paper_results.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 pes_data <- readRDS(file = "lichess_data/pes_data_full.RDS")
+
 # Moderators: 
 # Opp move time (opp_move_time_posterror)
 # Remaining time on player click (clock_secs_posterror)
@@ -9,10 +10,118 @@ pes_data <- readRDS(file = "lichess_data/pes_data_full.RDS")
 # Elo rating (player_moving_elo_posterror)
 
 source("helper_functions.R")
-plot_moderator_analysis(pes_data %>% filter(time_format == "bullet"), player_moving_elo_posterror)
-plot_moderator_analysis(pes_data %>% filter(time_format == "blitz"), player_moving_elo_posterror)
-plot_moderator_analysis(pes_data %>% filter(time_format == "rapid"), player_moving_elo_posterror)
+# plot_moderator_analysis(pes_data %>% filter(time_format == "bullet"), opp_move_time_secs_posterror)
+# plot_moderator_analysis(pes_data %>% filter(time_format == "blitz"), opp_move_time_secs_posterror)
+# plot_moderator_analysis(pes_data %>% filter(time_format == "rapid"), opp_move_time_secs_posterror)
+# plot_moderator_analysis(pes_data %>% filter(time_format == "classical"), opp_move_time_secs_posterror)
 
-# plot_moderator_grouped(pes_data, player_moving_elo_posterror)
+### Opp Move time -----
+# Extract each panel as its own ggplot
+plots_move_time <- lapply(split(pes_data, factor(pes_data$time_format, levels = c("bullet", "blitz", "rapid", "classical"))), function(subdf) {
+  plot_moderator_analysis(subdf, opp_move_time_secs_posterror)
+})
 
-# this is shit. Make them separate
+# Combine and label with A–D
+combined_move_time <- cowplot::plot_grid(
+  plots_move_time[[1]], plots_move_time[[2]], plots_move_time[[3]], plots_move_time[[4]],
+  labels = c("A", "B", "C", "D"),
+  label_size = 18,
+  ncol = 2
+)
+
+# Save the combined plot
+ggplot2::ggsave(
+  filename = "imgs/move_time_moderator.jpg",
+  plot = combined_move_time,
+  width = 16,       # adjust width as needed
+  height = 9,       # adjust height as needed
+  dpi = 300         # high-quality resolution for publication
+)
+
+### Time pressure -----
+plots_time_pressure <- lapply(split(pes_data, factor(pes_data$time_format, levels = c("bullet", "blitz", "rapid", "classical"))), function(subdf) {
+  plot_moderator_analysis(subdf, clock_secs_posterror)
+})
+
+# Combine and label with A–D
+combined_time_pressure <- cowplot::plot_grid(
+  plots_time_pressure[[1]], plots_time_pressure[[2]], plots_time_pressure[[3]], plots_time_pressure[[4]],
+  labels = c("A", "B", "C", "D"),
+  label_size = 18,
+  ncol = 2
+)
+
+# Save the combined plot
+ggplot2::ggsave(
+  filename = "imgs/time_pressure_moderator.jpg",
+  plot = combined_time_pressure,
+  width = 16,       # adjust width as needed
+  height = 9,       # adjust height as needed
+  dpi = 300         # high-quality resolution for publication
+)
+
+### Error severity -----
+plots_error_severity <- lapply(split(pes_data, factor(pes_data$time_format, levels = c("bullet", "blitz", "rapid", "classical"))), function(subdf) {
+  plot_moderator_analysis(subdf, prev_own_move_eval_posterror)
+})
+
+# Combine and label with A–D
+combined_error_severity <- cowplot::plot_grid(
+  plots_error_severity[[1]], plots_error_severity[[2]], plots_error_severity[[3]], plots_error_severity[[4]],
+  labels = c("A", "B", "C", "D"),
+  label_size = 18,
+  ncol = 2
+)
+
+# Save the combined plot
+ggplot2::ggsave(
+  filename = "imgs/error_severity_moderator.jpg",
+  plot = combined_error_severity,
+  width = 16,       # adjust width as needed
+  height = 9,       # adjust height as needed
+  dpi = 300         # high-quality resolution for publication
+)
+
+### Error speed -----
+plots_error_speed <- lapply(split(pes_data, factor(pes_data$time_format, levels = c("bullet", "blitz", "rapid", "classical"))), function(subdf) {
+  plot_moderator_analysis(subdf, prev_own_move_time_posterror)
+})
+
+# Combine and label with A–D
+combined_error_speed <- cowplot::plot_grid(
+  plots_error_speed[[1]], plots_error_speed[[2]], plots_error_speed[[3]], plots_error_speed[[4]],
+  labels = c("A", "B", "C", "D"),
+  label_size = 18,
+  ncol = 2
+)
+
+# Save the combined plot
+ggplot2::ggsave(
+  filename = "imgs/error_speed_moderator.jpg",
+  plot = combined_error_speed,
+  width = 16,       # adjust width as needed
+  height = 9,       # adjust height as needed
+  dpi = 300         # high-quality resolution for publication
+)
+
+### Player ability -----
+plots_player_ability <- lapply(split(pes_data, factor(pes_data$time_format, levels = c("bullet", "blitz", "rapid", "classical"))), function(subdf) {
+  plot_moderator_analysis(subdf, player_moving_elo_posterror)
+})
+
+# Combine and label with A–D
+combined_player_ability <- cowplot::plot_grid(
+  plots_player_ability[[1]], plots_player_ability[[2]], plots_player_ability[[3]], plots_player_ability[[4]],
+  labels = c("A", "B", "C", "D"),
+  label_size = 18,
+  ncol = 2
+)
+
+# Save the combined plot
+ggplot2::ggsave(
+  filename = "imgs/player_ability_moderator.jpg",
+  plot = combined_player_ability,
+  width = 16,       # adjust width as needed
+  height = 9,       # adjust height as needed
+  dpi = 300         # high-quality resolution for publication
+)
